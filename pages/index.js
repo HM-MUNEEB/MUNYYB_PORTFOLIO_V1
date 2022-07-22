ef54052da8939b68fa665b55d9f1a686e5b75898
HM Muneeb <hm.muneeb.u.r@gmail.com>
2022-07-23 00:00:03 +0500

    Organized in vector Image in components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import VectorImage from "../Components/vectorImage";
+import VectorImage from "../Components/vectorImage/vectorImage";
 
 export default function Home() {
   return (
