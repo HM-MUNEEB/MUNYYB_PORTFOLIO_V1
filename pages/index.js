a47d7283f5b6b2c1c76279fcfbe11cdd1e312a02
HM Muneeb <hm.muneeb.u.r@gmail.com>
2022-07-23 00:20:34 +0500

    Organized into phases for each iteration of animation - PART 2

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Phase2Module from "../Components/Phase2/Phase2Module";
+import Phase1Module from "../Components/Phase1/Phase1Module";
 
 export default function Home() {
   return (
@@ -12,7 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.mainContainer}>
-        <Phase2Module />
+        <Phase1Module />
+        {/* <Phase2Module /> */}
       </div>
     </div>
   );
